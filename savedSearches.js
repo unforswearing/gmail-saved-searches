f3d39e5bff3f58be150fc235ffa00c9818d4d823
Alvin <al.charity@gmail.com>
2021-04-05 23:43:29 -0400

    addt'l cleanup, add comments, using test.gs

--- savedSearches.js
+++ savedSearches.js
@@ -2,11 +2,15 @@
 
   SavedSearches: Add a hacky way to use labels as saved searches in gmail
   - started: 3/31/2021
+  -> possible solution for naming searches: https://www.inboxsdk.com/
+
 */
 class SavedSearches {
   constructor() {
+    // lgmail = local gmail
     this._lgmail = GmailApp;
 
+    // params will be used as an output to this class
     this.params = { 
       label: {
         saved: [],
@@ -17,7 +21,6 @@ class SavedSearches {
           prefix: 'saved_search>'
         }
       },
-      saved_search_re: new RegExp(/^saved_search:(!?=:)/, 'gim'),
       find_user_label: (name) => this._lgmail.getUserLabelByName(name),
       create_and_add_label: (name) => {
         this._lgmail.createLabel(name);
@@ -25,19 +28,66 @@ class SavedSearches {
       },
     };
 
+    // if the 'create' label doesn't exist create it, otherwise search for it
     this.params.label.object.create = (!this.params.find_user_label(this.params.label.string.create)) ? 
       this.params.create_and_add_label(this.params.label.string.create) :
       this.params.find_user_label(this.params.label.string.create);
 
+    // if the 'create' label doesn't exist create it, otherwise search for it
     this.params.label.object.remove = (!this.params.find_user_label(this.params.label.string.remove)) ?
       this.params.create_and_add_label(this.params.label.string.remove) :
       this.params.find_user_label(this.params.label.string.remove);
     
+    // use the output of the filters as data for 
+    // filter_saved / filter_create / and filter_remove params
+    this.params['saved_search_re'] = new RegExp(this.params.label.string.prefix, 'gim');
+    this.params['create_label_re'] = new RegExp(this.params.label.string.create, 'gim');
+    this.params['remove_label_re'] = new RegExp(this.params.label.string.remove, 'gim');
+
     this._searches = {};
     
+    // get the full label list (as gmailLabel)
+    // this only exists to retrieve the label name, so maybe 
+    // just create an array of name strings
     this._searches['list'] = this._lgmail.getUserLabels();
-    this._searches['filter_create'] = () => {};
-    this._searches['filter_remove'] = () => {};
+
+    // filter searches 
+    // these create/remove/saved filters can be deduped
+    this._searches['filter_create'] = () => {
+      let labelList = this._searches.list;
+      let getName = (label) => label.getName();
+
+      let collector = [];
+      
+      for (let i = 0; i < labelList.length; i++) {
+        let item = labelList[i];
+        let localItem = getName(item);
+
+        let isSaved = (localItem.match(this.params.create_label_re));
+        
+        collector.push(isSaved ? item : undefined);
+      };
+      
+      return collector.filter(Boolean);
+    };
+    
+    this._searches['filter_remove'] = () => {
+      let labelList = this._searches.list;
+      let getName = (label) => label.getName();
+
+      let collector = [];
+      
+      for (let i = 0; i < labelList.length; i++) {
+        let item = labelList[i];
+        let localItem = getName(item);
+
+        let isSaved = (localItem.match(this.params.remove_label_re));
+        
+        collector.push(isSaved ? item : undefined);
+      };
+      
+      return collector.filter(Boolean);
+    };
     
     this._searches['filter_saved'] = () => {
       let labelList = this._searches.list;
@@ -56,24 +106,20 @@ class SavedSearches {
       
       return collector.filter(Boolean);
     };
-    
-    this._searches['saved_searches'] = this._searches.filter_saved()
+  
+    this._searches['saved_searches'] = this._searches.filter_saved();
+    this._searches['create_search'] = this._searches.filter_create();
+    this._searches['remove_search'] = this._searches.filter_remove();
   };
 
   _parameters() { return this.params; };
-  labels() { return this._searches.saved_searches };
-  findNew() {};
-  add() {};
-  remove() {};
+  savedSearches() { return this._searches.saved_searches };
+  findCreate() { return this._searches.create_search };
+  findRemove() { return this._searches.remove_search };
 };
 
 function manageSavedSearches() {
-  let savesrch = new SavedSearches();
-  let labels = savesrch.labels();
 
-  Logger.log(savesrch)
-  Logger.log(labels)
-  Logger.log(savesrch._parameters())
 
   /*
     manage labels in this function - use on trigger, 
--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 function test() {
-  
+  let savesrch = new SavedSearches();
+  // let labels = savesrch.labels();
+  // add() {};
+  // remove() {};
+
+  Logger.log(savesrch.findCreate())
+  Logger.log(savesrch.findRemove())
+  Logger.log(savesrch.savedSearches())
 }
