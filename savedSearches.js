f945d3ab02f252e2012f42a72181346519155e53
Alvin <al.charity@gmail.com>
2021-04-05 22:53:22 -0400

    clean up code

--- savedSearches.js
+++ savedSearches.js
@@ -1,31 +1,19 @@
 /*
-  git: ~/Documents/__Github/gmail-saved-searches
+
   SavedSearches: Add a hacky way to use labels as saved searches in gmail
   - started: 3/31/2021
 */
 class SavedSearches {
-  /*
-    from: 3/31 - 4/1
-    @todo DO NOT OVER-ENGINEER THIS!
-    @todo save params.label to script properties
-    @todo add a list of saved searches (as strings and objects) to params.label.saved
-      as an object: { name1: { object, string }, name2: { ... }, ... }
-    @todo add newly created labels to params.label.saved
-    @todo when finding msgs labeled '::create' or '::remove', 
-      remove the create/remove label at some point after adding to script props
-    @todo need a way to allow 'user' to name the search
-    @todo might need a way to store the labels object outside of the script
-      (if there are a lot of saved searches / user labels to store) - 
-      firebase via firebase library?
-  */
   constructor() {
     this._lgmail = GmailApp;
+
     this.params = { 
       label: {
         saved: [],
         object: { create: false, remove: false, },
         string: {
-          create: 'saved_search::create', remove: 'saved_search::remove', 
+          create: 'saved_search::create', 
+          remove: 'saved_search::remove', 
           prefix: 'saved_search>'
         }
       },
@@ -36,31 +24,42 @@ class SavedSearches {
         this.params[name] = this.params.get_user_label(name);
       },
     };
+
     this.params.label.object.create = (!this.params.find_user_label(this.params.label.string.create)) ? 
       this.params.create_and_add_label(this.params.label.string.create) :
       this.params.find_user_label(this.params.label.string.create);
+
     this.params.label.object.remove = (!this.params.find_user_label(this.params.label.string.remove)) ?
       this.params.create_and_add_label(this.params.label.string.remove) :
       this.params.find_user_label(this.params.label.string.remove);
+    
     this._searches = {};
+    
     this._searches['list'] = this._lgmail.getUserLabels();
     this._searches['filter_create'] = () => {};
     this._searches['filter_remove'] = () => {};
+    
     this._searches['filter_saved'] = () => {
       let labelList = this._searches.list;
       let getName = (label) => label.getName();
+
       let collector = [];
+      
       for (let i = 0; i < labelList.length; i++) {
         let item = labelList[i];
         let localItem = getName(item);
+
         let isSaved = (localItem.match(this.params.saved_search_re));
+        
         collector.push(isSaved ? item : undefined);
       };
       
       return collector.filter(Boolean);
     };
+    
     this._searches['saved_searches'] = this._searches.filter_saved()
   };
+
   _parameters() { return this.params; };
   labels() { return this._searches.saved_searches };
   findNew() {};
