fe7e89f7da4a61d2060aeb724596bedb7da8d4db
Alvin <al.charity@gmail.com>
2021-06-27 19:34:04 -0400

    triggers will be the fuction executor

--- savedSearches.js
+++ savedSearches.js
@@ -27,24 +27,184 @@
 
 */
 
-const saveLabel = GmailApp.getUserLabels().find((label) => label.getName() === 'save-search')
-const delLabel = GmailApp.getUserLabels().find((label) => label.getName() === 'delete-search')
+// function createConfigSheet() {}
 
-const retrieveSearches = () => Config.retrieveSearches();
-const saveSearch = () =>  Config.addNewSearch();
+/** @type {Date} */
+const now = new Date().getTime();
 
-// this function only removes searches by gmail label
-// searches can also be removed by deleting the item in config sheet
-// see config.gs
-const removeSearch = () => Config.removeSearch();
+/** @type {string} */
+const datetime = `saved-search:${now}`;
 
-// currently searches can only be renamed in the config sheet 
-// or by manually editing the gmail label name.
-// this function handles any manually update gmail label names, 
-// see config for names updated in the sheet (uses onEdit trigger)
-// search for saved item, check if the name is the same as default 
-// listed in config, if not, update config. 
-const renameSearch = () => Config.renameSearch();
+/** @type {string} */
+const configSheetId = '1qVGGOCsHX9lhQcQ8-0VhpT2kf6l-2eNhDWEaGL4VICM'
+
+/** @type {object} */
+const spreadsheet = SpreadsheetApp.openById(globalThis.configSheetId)
+
+// 'store' uses script properties to store the saved search info
+/**
+ * @return {object}
+ */
+const store = () => {
+  /** @type {object} */
+  const props = PropertiesService().getScriptProperties();
+
+  return {
+    read: (key) => props.getProperty(key),
+    write: (key, value) => props.setProperty(key, value),
+  };
+};
+
+/** @return {Object} */
+const config_util = () => {
+  let config_params = {
+    // logging create_save_label alone will return an empty object
+    // use a label method (eg. getName()) to use the return value
+    create_save_label: () => GmailApp.createLabel('save-search'),
+    create_delete_label: () => GmailApp.createLabel('delete-search'),
+    /**
+     * @argument {object} infoObject
+     * @return {Array}
+     */
+    format_search_info_for_sheet: (infoObject) => {
+      let info = [
+        [
+          infoObject.datetime,
+          infoObject.messageIds,
+          "search terms",
+          infoObject.datetime,
+        ],
+      ];
+
+      return info
+    /**
+     * @type {object}
+     */
+    }, config_sheet: {
+      // columnHeader: ["datetime", "messageIds", "search", "title"],
+      // @todo fix this
+      lastRange: () => {
+        'A1:' + globalThis.spreadsheet.getLastRow() + globalThis.spreadsheet.getLastCol()
+      },
+    } 
+  }
+  return config_params
+};
+
+/** @return {Array} */
+function retrieveSavedSearches() {
+  let lastRange = config_util().config_sheet.lastRange()
+  const configData = spreadsheet.getRange(lastRange).getValues();
+  return configData
+}
+
+/** @return {object} */
+function findNewSavedSearchMessages() {
+
+  /** @return {Array} */
+  const getThreadIds = (label) => {
+    let labelThreads = label.getThreads()
+    let collector = []
+    for (let i = 0; i < labelThreads.length; i++) {
+      let thread = labelThreads[i]
+      let threadMessages = GmailApp.getMessagesForThread(thread);
+      collector.push(threadMessages.map((item) => item.getId))
+    }
+    return collector
+  }
+
+  /** @type {Array} */
+  let labels = GmalApp.getUserLabels()
+  let collector = []
+
+  for (let i = 0; i < labels.length; i++) {
+    let l = labels[i]
+
+    if (l.getName() === 'save-search') {
+      collector.push({
+        datetime: now,
+        messageIds: getThreadIds(),
+        searchTerm: undefined,
+        title: datetime
+      })
+    }
+  }
+
+  return collector
+}
+
+/**
+ * @argument {object} searchInfoObject
+ * @return {void}
+ */
+function addNewSavedSearch(searchInfoObject) {
+  // use ..format_mesage_info_for_sheet to help format info
+  // for configuration sheet
+  const labels = config_util().user_labels()
+  let newSavedSearches = [];
+  for (var n in labels) {
+    let label = labels[n];
+    if (label.getName() === 'save-search') newSavedSearches.push(label);
+  }
+
+  if (!newSavedSearches) return;
+  // generate info object, create default name, etc
+
+  /*
+    { 
+    datetime: yyy-mm-dd-hh:mm, 
+    messageids: [], 
+    search: <replicate search using msg ids>, 
+    name: `saved-search:${datetime}`
+  }
+  */
+
+  let container = {};
+
+  let tmpObj = {
+    /** @type {Date} */
+    datetime: now,
+    /** @type {string} */
+    search: undefined,
+    /** @type {Array} */
+    messageIds: [],
+    /** @type {string} */
+    title: datetime,
+  };
+
+  /** @type {object} */
+  container[now] = tmpObj;
+
+  store().write(datetime, container)
+}
+
+// @todo finish this once the basic functions work (add/delete searches)
+/**
+ * @argument {object} searchInfoObject
+ * @return {object}
+ */
+function renameSavedSearch(searchInfoObject) {
+  // update search name from default to user specified
+  // this function only applies to names changed in the config sheet.
+  // for names changed by manually updating the label, see savedSearches.gs
+}
+
+/**
+ * @argument {object} searchInfoObject
+ * @return {object}
+ */
+function removeSavedSearch(searchInfoObject) {
+  // this function only acts on searches removed from the spreadsheet
+}
+
+const config = {
+  sheetId: globalThis.configSheetId,
+  sheet: globalThis.spreadsheet,
+  retrieveSearches: retrieveSavedSearches,
+  addNewSearch: addNewSavedSearch,
+  renameSearch: renameSavedSearch,
+  removeSearch: removeSavedSearch,
+}
 
 
 
--- triggers.js
+++ triggers.js
@@ -1,3 +1,8 @@
+const scriptLabels = { 
+  save: GmailApp.getUserLabels().find((label) => label.getName() === 'save-search'),
+  del: GmailApp.getUserLabels().find((label) => label.getName() === 'delete-search'),
+}
+
 function onEdit() {
   // check if the range is the name col
   // check if each name contains the default name
@@ -6,6 +11,7 @@ function onEdit() {
 }
 
 function updateGmailLabelTitle() {
+
 }
 
 function findNewOrDeletedSearches() {
@@ -13,4 +19,46 @@ function findNewOrDeletedSearches() {
   // i could maybe have multiple of these in order to check 
   // every 5-10 seconds or so (5 seconds = 12 triggers, 
   // 10 seconds = 6 triggers)
+
+    /** @type {Array} */
+  let newlySaved = findNewSavedSearchMessages()
+  if (newlySaved) {
+    for (var i = 0; i < newlySaved.length; i++) {
+      // create searchinfo object
+      // addNewSavedSearch(searchInfoObject)
+    }
+  }
 }
+
+// this function only removes searches by gmail label
+// searches can also be removed by deleting the item in config sheet
+// see config.gs
+const removeSearch = '';
+
+// currently searches can only be renamed in the config sheet 
+// or by manually editing the gmail label name.
+// this function handles any manually update gmail label names, 
+// see config for names updated in the sheet (uses onEdit trigger)
+// search for saved item, check if the name is the same as default 
+// listed in config, if not, update config. 
+const renameSearch = '';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
